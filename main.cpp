87656aaf3c557b71e77392f0e3c005246fa29d5b
matthewpoletin <contact@matthewpoletin.ru>
2016-03-15 13:17:55 +0300

    Empty cpp project
    .gitignore for as

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * podzol.cpp
+ *
+ * Created: 15.03.2016 13:06:25
+ * Author : matt
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
